2be99c9df8a43637ec5ff8d8c73a45ffce3f0e44
GurjinderSinghMahal <50635244+GurjinderSinghMahal@users.noreply.github.com>
2020-08-04 23:43:33 -0400

    Add files via upload

---
+++ assets/data/myData.ts
@@ -0,0 +1,8 @@
+import { MahalGur } from "../../app/mahalgur"
+export const Info: MahalGur={
+    number: 991544274,
+    name:'Gurjinder Mahal',
+    loginName:'mahalgur',
+    campusName:'Davis',
+    assignmentTitle:'Assigment-4',
+} ;
